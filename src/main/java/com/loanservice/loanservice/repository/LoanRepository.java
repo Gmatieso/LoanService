58906d509167c3732c1692c5964329c8eff270f1
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2024-02-28 13:01:57 +0300

    loanService done

---
+++ src/main/java/com/loanservice/loanservice/repository/LoanRepository.java
@@ -0,0 +1,6 @@
+package com.loanservice.loanservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface loanRepository extends JpaRepository<Loan> {
+}
--- src/main/java/com/loanservice/loanservice/service/loanService.java
+++ src/main/java/com/loanservice/loanservice/service/loanService.java
@@ -1,4 +1,29 @@
 package com.loanservice.loanservice.service;
 
+import com.loanservice.loanservice.model.LoanRequest;
+import com.loanservice.loanservice.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class loanService {
+
+    @Autowired
+    private final LoanRepository loanRepository;
+
+
+    public loanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
+public LoanRequest createLoan(LoanRequest loanRequest){
+        LoanRequest loanSaved = loanRepository.save(loanRequest);
+
+        // send notification here
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForObject("",loanRequest, Void.class);
+    return loanSaved;
+
+    }
 }
